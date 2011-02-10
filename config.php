ad3024e6f56bf1d1be52a9a64d8d38cf4630f484
david.mercereau <david.mercereau@gmail.com>
2011-02-10 14:52:20 +0000

    version: 1.1

--- config.php
+++ config.php
@@ -1,15 +1,27 @@
 <?php
 
 // My Domains
-$mydomain1='freeserver.kr';
-$mydomain2='freeserver.kr';
+$mydomain_tab=array('calyce.fr', 'freeserver.kr');
+
+// TTL default value
+$ttl_default=array( 'A' => '7200',
+					     'AAAA' => '7200',
+					     'CNAME' => '7200',
+					     'NS' => '60',
+					     'TXT' => '7200',
+     					'PTR' => '7200',
+		     			'MX' => '3600');
 
 // Nameserver
 $ns1='211.208.163.118';
+
+// File for authCode (chmod 777 $file)
+$file = '/home/cslab/public_html/dnsupdate/.htpasswd';
 
 // Version Information
 $version='Ver. 20080606 Rev.2';
 
 // Copyright
-$copyright='&copy; 2008 <U>Computer Science Laboratory Network</U> (CSLab.net) All rights reserved.';
+$copyright='&copy; 2008 <u>Computer Science Laboratory Network</u> (CSLab.net) All rights reserved. -  
+<a href="http://validator.w3.org/check?uri=referer">Valid XHTML 1.0 Transitional</a>  <a href="http://jigsaw.w3.org/css-validator/check/referer">Valid CSS! </a>';
 ?>
